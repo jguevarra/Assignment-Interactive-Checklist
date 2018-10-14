aacaa1c76d0e685d2e6664c881f0fa1260eb3483
jrfguevarra <jfg3xs@virginia.edu>
2018-10-14 17:33:07 -0400

    created the index.html page. Only has 1 bullet point link to an example post

--- mysite/home/urls.py
+++ mysite/home/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /home/
     path('', views.index, name='index'),
+    # ex: /home/1/
+    path('<int:question_id>/', views.detail, name='detail'),
 ]
--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import loader
+from .models import Post
 
 
 def index(request):
-    return HttpResponse("Welcome home, we've been expecting you!") 
+    # displays the latest 5 poll questions in the system according to the post date
+    latest_post_list = Post.objects.order_by('-post_date')[:5]
+    # output = ', '.join([p.post_title for p in latest_post_list])
+    template = loader.get_template('home/index.html')
+    context = {
+        'latest_post_list': latest_post_list
+    }
+    return HttpResponse(template.render(context, request))
+
+# additional views
+def detail(request, post_id):
+    return HttpResponse("You're looking at question %s." % post_id)

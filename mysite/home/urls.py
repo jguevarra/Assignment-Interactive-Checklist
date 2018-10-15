38a51c6d8b383799e20cf69c7e7a94dbbe1636cd
jrfguevarra <jfg3xs@virginia.edu>
2018-10-14 20:36:28 -0400

    edited the detail.html page to include a header and placeholder text

--- mysite/home/urls.py
+++ mysite/home/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'assignment_calendar'
+
 urlpatterns = [
     # ex: /home/
     path('', views.index, name='index'),
--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.http import Http404
 from django.template import loader
@@ -15,8 +15,10 @@ def index(request):
 
 # additional views
 def detail(request, post_id):
-    try:
-        post = Post.objects.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404("Post does not exist")
-    return render(request, 'home/detail.html', {'post': post})
+    # try:
+    #     post = Post.objects.get(pk=post_id)
+    # except Post.DoesNotExist:
+    #     raise Http404("Post does not exist")
+    # return render(request, 'home/detail.html', {'post': post})
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'home/detail.html',{'post': post})

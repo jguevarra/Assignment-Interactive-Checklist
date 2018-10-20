f99f8b3334b34d3c888f2d57ac3e4c4f05f49bf3
jrfguevarra <jfg3xs@virginia.edu>
2018-10-20 16:16:46 -0400

    updated /calendar/ page

--- uni_assignment_calendar/urls.py
+++ uni_assignment_calendar/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /calendar/
     path('', views.index, name='index'),
+    # ex: /calendar/1/
+    path('<int:post_id>/', views.detail, name='detail'),
 ]
--- uni_assignment_calendar/views.py
+++ uni_assignment_calendar/views.py
@@ -1,4 +1,14 @@
 from django.http import HttpResponse
+from .models import Post
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the calendar index.")
+    latest_post_list = Post.objects.order_by('-post_date')[:5]
+    template = loader.get_template('uni_assignment_calendar/index.html')
+    context = {
+        'latest_post_list': latest_post_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, post_id):
+    return HttpResponse("You're looking at post # %s." % post_id)

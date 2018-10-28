04f8fa511ee2b89f9c9d459822c9b8198fdb80d6
jrfguevarra <jfg3xs@virginia.edu>
2018-10-27 22:09:00 -0400

    fixed test case

--- uni_assignment_calendar/tests.py
+++ uni_assignment_calendar/tests.py
@@ -6,27 +6,28 @@ from .models import Assignment
 import datetime
 
 
-# def create_event(event_text, days):
-#     """
-#     Create a question with the given `question_text` and published the
-#     given number of `days` offset to now (negative for questions published
-#     in the past, positive for questions that have yet to be published).
-#     """
-#     time = timezone.now() + datetime.timedelta(days=days)
-#     return Event.objects.create(event_name=event_text, day=time)
+def create_assignment(assignment_text, days):
+    """
+    Create a question with the given `question_text` and published the
+    given number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Assignment.objects.create(assignment_name=assignment_text, due_date=time)
 
 
 # Testing Index View
-class EventIndexViewTests(TestCase):
-    def test_no_events(self):
+class AssignmentIndexViewTests(TestCase):
+    def test_no_assignments(self):
         """
         If no post exist, an appropriate message is displayed.
         """
         response = self.client.get(reverse('calendar:index'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "No events have been posted.")
+        self.assertContains(response, "No assignments have been posted.")
 
-    # def test_future_events(self):
+
+    # def test_future_assignemnts(self):
     #     """
     #     Post with a date in the future aren't displayed on
     #     the index page.
@@ -36,11 +37,15 @@ class EventIndexViewTests(TestCase):
     #
     # def test_past_events(self):
     #     """
-    #     Post with a date in the past aren't displayed on
-    #     the index page.
+    #     Questions with a pub_date in the past are displayed on the
+    #     index page.
     #     """
+    #     create_assignment(assignment_text="Past assignment", days=-30)
     #     response = self.client.get(reverse('calendar:index'))
-    #     self.assertContains(response, "No events have been posted.")
+    #     self.assertQuerysetEqual(
+    #         response.context['latest_question_list'],
+    #         ['<Question: Past question.>']
+    #     )
 
 
 # test for past posts, recent posts, future posts

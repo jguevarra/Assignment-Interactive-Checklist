dc6e4af03b62d59daccfc2ceb4c7cd6329d74b5e
jrfguevarra <jfg3xs@virginia.edu>
2018-10-28 12:56:43 -0400

    more test cases

--- uni_assignment_calendar/models.py
+++ uni_assignment_calendar/models.py
@@ -36,7 +36,8 @@ class Assignment(models.Model): # change all Events to Assignments
 
     # returns most recent published post
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     class Meta:
         verbose_name = u'Scheduling'
--- uni_assignment_calendar/tests.py
+++ uni_assignment_calendar/tests.py
@@ -16,6 +16,37 @@ def create_assignment(assignment_text, days):
     return Assignment.objects.create(assignment_name=assignment_text, due_date=time)
 
 
+# Testing Model View
+class AssignmentnModelTests(TestCase):
+
+    def test_was_published_recently_with_future_assignment(self):
+        """
+        was_published_recently() returns False for assignment whose pub_date
+        is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Assignment(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_assignment(self):
+        """
+        was_published_recently() returns False for assignments whose pub_date
+        is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Assignment(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_assignment(self):
+        """
+        was_published_recently() returns True for assignments whose pub_date
+        is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Assignment(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
+
+
 # Testing Index View
 class AssignmentIndexViewTests(TestCase):
     def test_no_assignments(self):
@@ -25,16 +56,17 @@ class AssignmentIndexViewTests(TestCase):
         response = self.client.get(reverse('calendar:index'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No assignments have been posted.")
-
-
+    #
     # def test_future_assignemnts(self):
     #     """
-    #     Post with a date in the future aren't displayed on
+    #     assignment with a date in the future aren't displayed on
     #     the index page.
     #     """
+    #     create_assignment(question_text="Future assignment.", days=30)
     #     response = self.client.get(reverse('calendar:index'))
-    #     self.assertContains(response, "No events have been posted.")
-    #
+    #     self.assertContains(response, "No assignments are available.")
+    #     self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
     # def test_past_events(self):
     #     """
     #     Questions with a pub_date in the past are displayed on the
@@ -47,6 +79,17 @@ class AssignmentIndexViewTests(TestCase):
     #         ['<Question: Past question.>']
     #     )
 
+# testing detail view
+# class QuestionDetailViewTests(TestCase):
+#     def test_future_assignment(self):
+#         """
+#         The detail view of a question with a pub_date in the future
+#         returns a 404 not found.
+#         """
+#         future_assignment = create_assignment(assignment_text='Future assignment.', days=5)
+#         url = reverse('calendar:detail', args=(future_assignment.id,))
+#         response = self.client.get(url)
+#         self.assertEqual(response.status_code, 404)
 
 # test for past posts, recent posts, future posts
 # test for future and past posts in the case of the index page having multiple posts

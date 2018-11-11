11de62ce9248b4ba3c883a434a26b6d412b89814
EJRIV <ejr8wh@virginia.edu>
2018-11-11 18:18:27 -0500

    Changed it so the landing page is correct now. Removes the extra /calendar/ field from the urls

--- cs3240_calendar/urls.py
+++ cs3240_calendar/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('calendar/', include('uni_assignment_calendar.urls')),
+    path('', include('uni_assignment_calendar.urls')),
     path('admin/', admin.site.urls),
 ]

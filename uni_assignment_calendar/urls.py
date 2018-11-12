a57a94268922673d19335b5aa9570ae25091bfee
Daniel Hanson <daniel.w.hanson1@gmail.com>
2018-11-12 12:53:58 -0500

    dummy commit

--- uni_assignment_calendar/urls.py
+++ uni_assignment_calendar/urls.py
@@ -15,5 +15,5 @@ urlpatterns = [
     path('accounts/login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('schedule/', views.schedule, name='schedule'),
-    parth('ScheduleResults/',views.ScheduleResults,name='ScheduleResults')
+    path('ScheduleResults/',views.ScheduleResults,name='ScheduleResults')
 ]

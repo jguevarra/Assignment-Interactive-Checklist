51e426e7c6f088eb6397707e3ac8b2756b45f14e
EJRIV <ejr8wh@virginia.edu>
2018-11-12 16:37:26 -0500

    hm

--- uni_assignment_calendar/models.py
+++ uni_assignment_calendar/models.py
@@ -24,8 +24,6 @@ class Events(models.Model):
     due_time = models.TimeField((u"Due Time"), blank=True, default="11:59")
     pub_date = models.DateTimeField(auto_now_add=True)
     description = models.TextField(blank=True, null=True)
-    class Meta:
-        ordering = ["class_title"]
     def __str__(self):
         return self.events_name
 

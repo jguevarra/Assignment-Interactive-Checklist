5d8de85495278812bdbec654cf1aab46e68c2ce3
EJRIV <ejr8wh@virginia.edu>
2018-11-12 16:41:42 -0500

    How about this one?

--- uni_assignment_calendar/models.py
+++ uni_assignment_calendar/models.py
@@ -13,6 +13,8 @@ class Courses(models.Model):
     class_title = models.TextField(max_length=50)
     instructor = models.CharField(max_length=200)
     description = models.TextField(max_length=500)
+    class Meta:
+        ordering = ["class_title"]
     def __str__(self):
         return self.class_title
 
@@ -24,8 +26,6 @@ class Events(models.Model):
     due_time = models.TimeField((u"Due Time"), blank=True, default="11:59")
     pub_date = models.DateTimeField(auto_now_add=True)
     description = models.TextField(blank=True, null=True)
-    class Meta:
-        ordering = ["course"]
     def __str__(self):
         return self.events_name
 

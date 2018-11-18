5553f180bb0398172ca716f6385d08aa02ab00c0
jrfguevarra <jfg3xs@virginia.edu>
2018-11-18 18:08:06 -0500

    fixed minor bugs in UI and added About Us and Home tabs in side bar

--- uni_assignment_calendar/urls.py
+++ uni_assignment_calendar/urls.py
@@ -17,4 +17,5 @@ urlpatterns = [
     path('logout/', views.user_logout, name='logout'),
     path('schedule/', views.schedule, name='schedule'),
     path('schedule/ScheduleResults/',views.ScheduleResults,name='ScheduleResults'),
+    path('AboutUs/', views.InfoViews, name='AboutUs'),
 ]

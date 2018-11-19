4aaba714ce8e27b0a6f56a47911a280f34d08b7c
EJRIV <ejr8wh@virginia.edu>
2018-11-19 13:43:22 -0500

    annoying dropdown menu...

--- uni_assignment_calendar/models.py
+++ uni_assignment_calendar/models.py
@@ -18,7 +18,7 @@ class Courses(models.Model):
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "class_id":
-            kwargs["queryset"] = Category.objects.filter(name__in=['14993'])
+            kwargs["queryset"] = Category.objects.filter(class_id__in=['14993'])
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
  #   def __str__(self):
  #       return self.class_title

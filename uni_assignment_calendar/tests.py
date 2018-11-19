ed6cf6eb2fa8a283e3b2d913724bd528165f11a2
KiriNicholson <32941940+KiriNicholson@users.noreply.github.com>
2018-11-19 15:32:40 -0500

    tests update

--- uni_assignment_calendar/tests.py
+++ uni_assignment_calendar/tests.py
@@ -251,7 +251,7 @@ class ScheduleTests(TestCase):
 	self.assertContains(response, "No enrolled courses")
 	
 
-    def test_class_info(self):
+    #def test_class_info(self):
 	"""
 	testing to make sure all of the info comes up in the class info page
 	"""
@@ -260,8 +260,8 @@ class ScheduleTests(TestCase):
 	response = self.client.get(reverse('course/15842'))
 	self.assertContains(response,"Diana Morris")
 
-"""
-    def test_if_enrolled_class_added(self):
+	"""
+    #def test_if_enrolled_class_added(self):
         """
         if the user enrolls in a class, the class is added in their schedule
 	manually made a specific user a enrolled in this class
@@ -272,7 +272,7 @@ class ScheduleTests(TestCase):
 	self.assertContains(response, "APMA 3140")
 
 
- #   def test_if_enrolled_assignment_shows(self):
+    #def test_if_enrolled_assignment_shows(self):
         """
         an assignment post in that class will be shown in the To Do
 	manually made a specific user enrolled in this class with this assignment
@@ -281,7 +281,7 @@ class ScheduleTests(TestCase):
 	c.login(user="a", password="a")
 	response = self.client.get(reverse('schedule'))
 	self.assertContains(response, "Test Assignment")
-"""
+	"""
 
    def test_if_enrolled_in_courses(self)
         c = Client()

6a3101bf2e01cdc7086192dbdd80dc97949099a1
ChristineCheng1213 <xc3fe@virginia.edu>
2018-12-02 23:33:37 -0500

    fix dropdown so can only post assignment for enrolled classes

--- uni_assignment_calendar/forms.py
+++ uni_assignment_calendar/forms.py
@@ -1,9 +1,10 @@
 from django import forms
-from .models import Events
+from .models import Events,Courses,Enrollment
 from django.contrib.auth.models import User
 import datetime
 
 class IndexForm(forms.ModelForm):
+    course = forms.ModelChoiceField(queryset=Courses.objects.all())
     due_date = forms.DateField(input_formats=["%m/%d/%Y"], widget=forms.DateInput(format=["%m/%d/%Y"]))
     due_time = forms.TimeField(widget=forms.TimeInput(format='%H:%M'))
     class Meta:

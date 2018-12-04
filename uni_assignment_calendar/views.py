221aff910c88aacd50b5d60a301f27284a8033f0
Daniel Hanson <daniel.w.hanson1@gmail.com>
2018-12-03 20:21:20 -0500

    commit

--- uni_assignment_calendar/views.py
+++ uni_assignment_calendar/views.py
@@ -136,7 +136,7 @@ def hideAssgn(request):
             i -= 1     
         hide = temp[::-1]
         enroll = Enrollment.objects.filter(username=request.user.username)
-        enroll.exclude.add(int(hide))
+        enroll.add(exclude=hide)
         message = 'Assignment deleted'
     else:
         message = 'Something went wrong!'

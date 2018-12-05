1120962a50008714038b007ad0e1fe890a48efd9
Daniel Hanson <daniel.w.hanson1@gmail.com>
2018-12-05 17:52:18 -0500

    hmm

--- uni_assignment_calendar/views.py
+++ uni_assignment_calendar/views.py
@@ -129,11 +129,8 @@ def schedule(request):
         #course = get_object_or_404(Courses,class_id=c.class_id)
         course = Courses.objects.get(class_id=c.class_id)
         for i in Events.objects.filter(course=course).order_by('due_date','due_time'):
-            try:
-                if request.user.username in i.users:
-                    events_list += i
-            except:
-                pass
+            if request.user.username in i.users:
+                events_list += i
         course_list.append(course)
 
     context = {'events_list':events_list,'enrollments':enrollments,'course_list':course_list}

73d0c2b06d7bab4d09d30efc48405201ef5a9d4c
Daniel Hanson <daniel.w.hanson1@gmail.com>
2018-12-05 12:17:05 -0500

    try except fix

--- uni_assignment_calendar/views.py
+++ uni_assignment_calendar/views.py
@@ -143,11 +143,12 @@ def hideAssgn(request):
             temp += mystring[i]
             i -= 1     
         hide = temp[::-1]
-        assgn = Events.objects.get(id = hide)
-        assgn.users.remove(request.user.username)
-        message = 'Assignment deleted'
-    else:
-        message = 'Something went wrong!'
+        try:
+            assgn = Events.objects.get(id = hide)
+            assgn.users.remove(request.user.username)
+            message = 'Assignment deleted'
+        except:
+            message = 'Something went wrong!'
     return HttpResponse(message)
 
 # Form for creating an assignment

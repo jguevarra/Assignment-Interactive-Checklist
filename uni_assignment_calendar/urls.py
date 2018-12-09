d03df36d52df1a0567d3421722ff6f14ae5edf9e
jguevarra <jfg3xs@virginia.edu>
2018-12-08 21:35:10 -0500

    transferring to do list UI into the index by changing the urls

--- uni_assignment_calendar/urls.py
+++ uni_assignment_calendar/urls.py
@@ -6,7 +6,7 @@ app_name = ''
 
 urlpatterns = [
     path('', views.user_login, name='login'),
-    path('home/', views.index, name='index'),
+    path('home/', views.schedule, name='index'),
     path('<int:events_id>/', views.detail, name='detail'),
     path('course/<int:class_id>/', views.course_detail, name='course_detail'),
     path('create/', views.create_assignment, name='create'),
@@ -14,7 +14,7 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('accounts/login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
-    path('schedule/', views.schedule, name='schedule'),
+    # path('schedule/', views.schedule, name='schedule'),
     path('schedule/ScheduleResults/',views.ScheduleResults,name='ScheduleResults'),
     path('AboutUs/', views.AboutView, name='AboutUs'),
     path('goals/', views.GoalsView, name='goals'),

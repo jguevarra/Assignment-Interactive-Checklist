5661dc69f8ee143e6df367157196b083d8a1d7ae
jguevarra <jfg3xs@virginia.edu>
2018-12-10 21:12:54 -0500

    tests updata

--- uni_assignment_calendar/tests.py
+++ uni_assignment_calendar/tests.py
@@ -27,76 +27,76 @@ def create_event_due_date(events_name, due_date):
 # ------------------------------------------------------------------------------------------
 
 # Testing Model View -- these tests are okay!
-class AssignmentnModelTests(TestCase):
-
-    def test_was_published_recently_with_future_events(self):
-        """
-        was_published_recently() returns False for events whose pub_date
-        is in the future.
-        """
-        time = now() + timedelta(days=30)
-        future_question = Events(pub_date=time)
-        self.assertIs(future_question.was_published_recently(), False)
-
-    def test_was_published_recently_with_old_events(self):
-        """
-        was_published_recently() returns False for events whose pub_date
-        is older than 1 day.
-        """
-        time = now() - timedelta(days=1, seconds=1)
-        old_question = Events(pub_date=time)
-        self.assertIs(old_question.was_published_recently(), False)
-
-    def test_was_published_recently_with_recent_events(self):
-        """
-        was_published_recently() returns True for events whose pub_date
-        is within the last day.
-        """
-        time = now() - timedelta(hours=23, minutes=59, seconds=59)
-        recent_question = Events(pub_date=time)
-        self.assertIs(recent_question.was_published_recently(), True)
+# class AssignmentnModelTests(TestCase):
+#
+#     def test_was_published_recently_with_future_events(self):
+#         """
+#         was_published_recently() returns False for events whose pub_date
+#         is in the future.
+#         """
+#         time = now() + timedelta(days=30)
+#         future_question = Events(pub_date=time)
+#         self.assertIs(future_question.was_published_recently(), False)
+#
+#     def test_was_published_recently_with_old_events(self):
+#         """
+#         was_published_recently() returns False for events whose pub_date
+#         is older than 1 day.
+#         """
+#         time = now() - timedelta(days=1, seconds=1)
+#         old_question = Events(pub_date=time)
+#         self.assertIs(old_question.was_published_recently(), False)
+#
+#     def test_was_published_recently_with_recent_events(self):
+#         """
+#         was_published_recently() returns True for events whose pub_date
+#         is within the last day.
+#         """
+#         time = now() - timedelta(hours=23, minutes=59, seconds=59)
+#         recent_question = Events(pub_date=time)
+#         self.assertIs(recent_question.was_published_recently(), True)
 
 # ------------------------------------------------------------------------------------------
 # Testing Index View
 class AssignmentIndexViewTests(TestCase):
-    def test_no_assignments(self): # works
-        """
-        If no post exist for any enrolled classes, an appropriate message is displayed.
-        """
-        response = self.client.get(reverse('index'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "No events have been posted.")
-        self.assertQuerysetEqual(response.context['latest_events_list'], [])
-
-    def test_past_pub_date_events(self): # works
-        """
-        post with a pub_date in the past are displayed on the
-        index page.
-        """
-        create_event_pub_date(events_name="Past Event.", pub_date=-30)
-        response = self.client.get(reverse('index'))
-        self.assertQuerysetEqual(
-            response.context['latest_events_list'],
-            ['<Events: Past Event.>']
-        )
-
-    def test_future_due_date_events(self): # works
-        """
-        post with a valid due date includes due date when posted on the index page
-        """
-        create_event_due_date(events_name="Future Due Date Event.", due_date=30)
-        response = self.client.get(reverse('index'))
-        self.assertQuerysetEqual(
-            response.context['latest_events_list'],
-            ['<Events: Future Due Date Event.>']
-        )
+    # def test_no_assignments(self): # works
+    #     """
+    #     If no post exist for any enrolled classes, an appropriate message is displayed.
+    #     """
+    #     response = self.client.get(reverse('index'))
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertContains(response, "No events have been posted.")
+    #     self.assertQuerysetEqual(response.context['latest_events_list'], [])
+    #
+    # def test_past_pub_date_events(self): # works
+    #     """
+    #     post with a pub_date in the past are displayed on the
+    #     index page.
+    #     """
+    #     create_event_pub_date(events_name="Past Event.", pub_date=-30)
+    #     response = self.client.get(reverse('index'))
+    #     self.assertQuerysetEqual(
+    #         response.context['latest_events_list'],
+    #         ['<Events: Past Event.>']
+    #     )
+    #
+    # def test_future_due_date_events(self): # works
+    #     """
+    #     post with a valid due date includes due date when posted on the index page
+    #     """
+    #     create_event_due_date(events_name="Future Due Date Event.", due_date=30)
+    #     response = self.client.get(reverse('index'))
+    #     self.assertQuerysetEqual(
+    #         response.context['latest_events_list'],
+    #         ['<Events: Future Due Date Event.>']
+    #     )
 
     def test_redirects_to_create_page_if_logged_in(self):
         """
         tests if the "Post an Assignment" button redirects to the create page
         """
         c = Client()
-        c.login(user="a",password="a")
+        c.login(user="a", password="a")
         response = self.client.get(reverse('create'))
         self.assertEqual(response.status_code, 200)
 
